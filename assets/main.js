d4641bcba042ffd83e41d99a52acc0a204c29577
Valentina De Mite <valesakura97@gmail.com>
2023-09-20 15:58:56 +0200

    added e-mails generator

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,17 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      emails: [],
     };
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+          console.log(resp);
+          this.emails.push(resp.data.response);
+        });
+    }
   },
 }).mount("#app");
